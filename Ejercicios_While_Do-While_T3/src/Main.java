8ff53b15675fb56a32730c33518bf03a9501ed87
Jheremi Lema <jheremisj9695@gmail.com>
2025-11-09 01:24:56 +0100

    modificación comentarios

--- Ejercicios_While_Do-While_T3/src/Main.java
+++ Ejercicios_While_Do-While_T3/src/Main.java
@@ -6,15 +6,15 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
         Ejercicios operaciones = new Ejercicios(teclado);
 
-        //operaciones.ejercicio1();
-        //operaciones.ejercicio2();
-        //operaciones.ejercicio3();
-        //operaciones.ejercicio4();
-        //operaciones.ejercicio5();
-        //operaciones.ejercicio6();
-        //operaciones.ejercicio7();
-        //operaciones.ejercicio8();
-        //operaciones.ejercicio9();
+        operaciones.ejercicio1();
+        operaciones.ejercicio2();
+        operaciones.ejercicio3();
+        operaciones.ejercicio4();
+        operaciones.ejercicio5();
+        operaciones.ejercicio6();
+        operaciones.ejercicio7();
+        operaciones.ejercicio8();
+        operaciones.ejercicio9();
         operaciones.ejercicio10();
 
         teclado.close();
